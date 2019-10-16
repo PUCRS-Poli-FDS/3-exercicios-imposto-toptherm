99fd7c09d6647977ba3b8967460d4803f455099f
felipefahrion <PFahrion1998>
2019-10-15 21:01:56 -0300

    inicia tela

--- src/controller/TelaInicialController.java
+++ src/controller/TelaInicialController.java
@@ -5,14 +5,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
-public class TelaInicialController extends Application
-{
+public class TelaInicialController extends Application {
 
     @FXML
     private TextField nome;
@@ -32,14 +32,15 @@ public class TelaInicialController extends Application
     @FXML
     private TextField rendimentoTotal;
 
+
     @Override
     public void start(Stage stage) throws IOException {
-        URL resource = getClass().getClassLoader().getResource("/fxml/telaCadastro.fxml");
-        URL arquivoFXML = resource;
-        Parent fxmlParent = FXMLLoader.load(arquivoFXML);
+        File f = new File("src/fxml/telaCadastro.fxml");
+        Parent fxmlParent = FXMLLoader.load(f.toURI().toURL());
+
         stage.setScene(new Scene(fxmlParent, 300, 100));
-        stage.setTitle("Olá mundo com FXML");
         stage.show();
+
     }
 
     public static void main(String[] args) {
